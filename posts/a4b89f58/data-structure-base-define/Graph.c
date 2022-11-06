d770dbbf4a5be6031dc6f4ace6571a7f0ce7d9c1
couriourc <3234853521@qq.com>
2022-11-06 13:28:05 +0800

    Site updated: 2022-11-06 13:28:03

--- posts/a4b89f58/data-structure-base-define/Graph.c
+++ posts/a4b89f58/data-structure-base-define/Graph.c
@@ -49,6 +49,7 @@
 //
 //}
 #include <stdio.h>
+#include <intrin.h>
 
 typedef struct Node {
     int data;
@@ -56,25 +57,44 @@ typedef struct Node {
     struct Node *right;
 } TreeNode, *Tree;
 
-int search(Tree root, int x, int res) {
+int search(Tree root, int x) {
+    int l, r, res = 0;
     if (!root) return res;
-    if (!(root->data) % x) {
-        search(root->left, x, res + 1);
-        search(root->right, x, res + 1);
-    } else {
-        search(root->left, x, res);
-        search(root->right, x, res);
-    }
-    return res;
+    if (!(root->data % x)) ++res;
+
+    l = search(root->left, x);
+    r = search(root->right, x);
+    // 数据整合
+    return res + l + r;
 }
 
 int main() {
-    Tree root,
-            l1 = NULL,
-            r1 = NULL,
-            l2 = NULL,
-            r2 = NULL;
 
-    search(root, 2, 0);
+    Tree root = (Tree) malloc(sizeof(TreeNode)),
+            l1 = (Tree) malloc(sizeof(TreeNode)),
+            r1 = (Tree) malloc(sizeof(TreeNode)),
+            l2 = (Tree) malloc(sizeof(TreeNode)),
+            r2 = (Tree) malloc(sizeof(TreeNode));
+    root->data = 0;
+    l1->data = 1;
+    r1->data = 2;
+    l2->data = 3;
+    r2->data = 4;
+    root->left = l1;
+    root->right = r1;
+
+    l1->left = l2;
+    l1->right = NULL;
+    l2->left = NULL;
+    l2->right = NULL;
+
+    r1->right = r2;
+    r1->left = NULL;
+    r2->left = NULL;
+    r2->right = NULL;
+
+
+    int res = search(root, 2);
+    printf("%d", res);
     return 0;
 }
