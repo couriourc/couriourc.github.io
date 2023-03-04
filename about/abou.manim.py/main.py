8ea7754ddf434392796bda135d88e839e8d07959
couriourc <3234853521@qq.com>
2023-03-04 08:48:44 +0800

    Site updated: 2023-03-04 08:48:44

---
+++ about/abou.manim.py/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3.6
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 #
+# @Time    : 2023/2/19 0019 15:01
+# @Author  : CouriourC
+# @Email   : godakid@outlook.com
+# @File    : main
+# @Software: PyCharm
+class PythonBlockHeader:
+    '''
+    :description: 版本号
+    '''
+    CURRENT_VERSION = 4
+    nVersion = None
+    hashPrevBlock = None
+    hashMerkleBlock = NOnesBlock
